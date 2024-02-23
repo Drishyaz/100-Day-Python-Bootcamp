86bbfae4febdecd7ab2eeb4d38f1a835e8d617a7
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2024-02-24 00:43:06 +0530

    Update Day-6-7-hangman.py

--- Day-6-7-hangman.py
+++ Day-6-7-hangman.py
@@ -1,13 +1,18 @@
+# LIVE DEMO: https://replit.com/@guardianblossom/Day-7-Hangman-5-Start#main.py
 import random
 from hangman_words import word_list
 from hangman_art import logo,stages
 
 print(logo)
+# Update the word list to use the 'word_list' from hangman_words.py
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
+
+# Import the logo from hangman_art.py and print it at the start of the game.
+
 # Testing code
 # print(f'Pssst, the solution is {chosen_word}.')
 
@@ -21,6 +26,7 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     # If the user has entered a letter they've already guessed, print the letter and let them know.
+
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
@@ -28,7 +34,7 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
 
-    # Check if the user is wrong.
+    # Check if user is wrong.
     if guess not in chosen_word:
         # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         lives -= 1
@@ -39,7 +45,7 @@ while not end_of_game:
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
-    # Check if the user has got all the letters.
+    # Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win!!")
