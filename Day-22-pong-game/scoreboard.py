c8345ec870a773d8644b91136e1fa6ceebc20914
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2024-03-12 23:04:35 +0530

    Update scoreboard.py

--- Day-22-pong-game/scoreboard.py
+++ Day-22-pong-game/scoreboard.py
@@ -2,8 +2,8 @@ from turtle import Turtle, Screen
 ALIGNMENT = "center"
 FONT = ("Courier", 48, "normal")
 
-
 class Scoreboard(Turtle):
+    # initializes the scoreboard => white colored, penup, hidden turtle, etc
     def __init__(self):
         super().__init__()
         self.hideturtle()
@@ -13,6 +13,7 @@ class Scoreboard(Turtle):
         self.r_score = 0
         self.update_scoreboard()
 
+    # updates the scoreboard as the points get updated
     def update_scoreboard(self):
         self.clear()
         self.goto(100, 200)
@@ -20,10 +21,12 @@ class Scoreboard(Turtle):
         self.goto(-100, 200)
         self.write(f"{self.r_score}", align=ALIGNMENT, font=FONT)
 
+    # updates the left side's score, and calls the update method to update the scoreboard
     def l_point(self):
         self.l_score += 1
         self.update_scoreboard()
 
+    # updates the right side's score, and calls the update method to update the scoreboard
     def r_point(self):
         self.r_score += 1
         self.update_scoreboard()
