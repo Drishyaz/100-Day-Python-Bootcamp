cd8b9be335d02780f53fabc64789f90c590c410c
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2024-03-12 23:29:06 +0530

    Update Day-11-blackjack.py

--- Day-11-blackjack/Day-11-blackjack.py
+++ Day-11-blackjack/Day-11-blackjack.py
@@ -1,9 +1,4 @@
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+# LIVE DEMO: https://replit.com/@guardianblossom/day-11-blackjack-start
 
 ############### Our Blackjack House Rules ####################
 
@@ -16,20 +11,6 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
 from art import logo
 print(logo)
