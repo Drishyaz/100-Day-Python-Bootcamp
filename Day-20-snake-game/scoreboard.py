a402439f050ea4e02dfd32e17f702fdc9510733d
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2024-03-15 23:26:33 +0530

    Update scoreboard.py

--- Day-20-snake-game/scoreboard.py
+++ Day-20-snake-game/scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
