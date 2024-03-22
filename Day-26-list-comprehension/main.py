7ca87487206f590d9e01090cbb4f8a2385c7e15b
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2024-03-22 23:34:04 +0530

    Update main.py

--- Day-26-list-comprehension/main.py
+++ Day-26-list-comprehension/main.py
@@ -66,3 +66,9 @@ sentence = input().split()       # What is the Airspeed Velocity of an Unladen S
 result = {word:len(word) for word in sentence}
 print(result)
 # result = {'What': 4, 'is': 2, 'the': 3, 'Airspeed': 8, 'Velocity': 8, 'of': 2, 'an': 2, 'Unladen': 7, 'Swallow?': 8}
+
+# TAKE EACH temp in celsius AND CONVERT IT INTO temp in fahrenhiet
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+weather_f = {day:(temp_c * 9/5) + 32 for (day,temp_c) in weather_c.items()}
+print(weather_f)
+# weather_f = {'Monday': 53.6, 'Tuesday': 57.2, 'Wednesday': 59.0, 'Thursday': 57.2, 'Friday': 69.8, 'Saturday': 71.6, 'Sunday': 75.2}
