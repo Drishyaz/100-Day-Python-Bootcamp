f5c168e06d9b9ffe91e4501628f08a237726567d
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2024-03-25 01:39:30 +0530

    Update main.py

--- Day-26-NATO-alphabet-start/main.py
+++ Day-26-NATO-alphabet-start/main.py
@@ -3,34 +3,32 @@
 # SO THE NATO PHONETIC CODES ARE USED TO NAME A WORD WITH THAT LETTER TO GET THE CALLER ON THE OTHER SIDE
 # UNDERSTAND IT. SO IF THE WORD IS JOY,THE CODES ARE ['Juliet', 'Oscar', 'Yankee']
 
-# LONGER AND SIMPLER VERSION
-# import pandas
-# data = pandas.read_csv("nato_phonetic_alphabet.csv")        # this is a dataframe
-# # TODO 1. Create a dictionary in this format:
-# # {"A": "Alfa", "B": "Bravo"}
-# data_dict = {}
-# for (index, row) in data.iterrows():
-#     data_dict[row.letter] = row.code
-# print(data_dict)
-#
-# # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# word = input("Type your word: ").upper()
-# phonetic_codes = []
-# for letter in word:
-#     code_word = data_dict[letter]
-#     phonetic_codes.append(code_word)
-# print(phonetic_codes)
+import pandas
+data = pandas.read_csv("nato_phonetic_alphabet.csv")          # this is a dataframe
 
 # SHORTER AND COMPLEX VERSION
-import pandas
-data = pandas.read_csv("nato_phonetic_alphabet.csv")        # this is a dataframe
-
 # TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
-data_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+data_dict = {row.letter:row.code for (index, row) in data.iterrows()}    # using dictionary comprehension
 print(data_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Type your word: ").upper()
-phonetic_codes = [data_dict[letter] for letter in word]
+phonetic_codes = [data_dict[letter] for letter in word]                  # using list comprehension
 print(phonetic_codes)
+
+# LONGER AND SIMPLER VERSION
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+# data_dict = {}
+# for (index, row) in data.iterrows():                        # looping through each row of the dataframe using iterrows()
+#     data_dict[row.letter] = row.code
+# print(data_dict)
+#
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# word = input("Type your word: ").upper()
+# phonetic_codes = []
+# for letter in word:
+#     code_word = data_dict[letter]                           # accessing each value of the dictionary with the key
+#     phonetic_codes.append(code_word)                        # and appending them to the 'phonetic_codes' list
+# print(phonetic_codes)
