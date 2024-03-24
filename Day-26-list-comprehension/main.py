bdf6f1ac82a23142a970e5d865e8d6d9cd845311
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2024-03-24 23:54:43 +0530

    Update main.py

--- Day-26-list-comprehension/main.py
+++ Day-26-list-comprehension/main.py
@@ -1,6 +1,6 @@
 # LIST COMPREHENSION
 # new_list = [new_item for item in list]
-# YOU ADD 1 TO EACH ITEM IN LIST
+# YOU ADD 1 TO EACH ITEM IN THE LIST
 numbers = [1,2,3]
 num_list = [n+1 for n in numbers]
 print(num_list)                                     # [2, 3, 4]
@@ -17,7 +17,7 @@ print(doubles)                                     # [2, 4, 6, 8]
 # CONDITIONAL LIST COMPREHENSION
 # new_list = [item for item in list if test]
 # PRINT ONLY THE NAMES WHICH HAVE LENGTH <= 4
-names = ["Alex","Beth","Caroline","Dave","Eleanor","Freddie"]
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_names = [name for name in names if len(name) <= 4]
 print(short_names)                                     # ["Alex", "Beth", "Dave"]
 
@@ -37,28 +37,28 @@ print(result)                                         # [2, 8, 34]
 # HARD CHALLENGE
 # CREATE A LIST result THAT HAS NUMBERS COMMON IN BOTH file1.txt and file2.txt
 with open("file1.txt", "r") as f1:
-  new_list = f1.readlines()                     # first we extract the contents of file1.txt
-list1 = [int(n.strip()) for n in new_list]      # then we convert each item into integer, and strip the newline
+  new_list = f1.readlines()                     # First we extract the contents of file1.txt
+list1 = [int(n.strip()) for n in new_list]      # Then we convert each item into an integer and strip the newline
 
 with open("file2.txt", "r") as f2:
-  new_list = f2.readlines()                     # next we extract the contents of file2.txt
-list2 = [int(n.strip()) for n in new_list]      # then we convert each item into integer, and strip the newline
+  new_list = f2.readlines()                     # Next we extract the contents of file2.txt
+list2 = [int(n.strip()) for n in new_list]      # Then we convert each item into an integer and strip the newline
 
 result = [n for n in list1 if n in list2]       # lastly we check for each item in list1, if present in list2
-                                                # only then we consider those items
+                                                # Only then do we consider those items
 
-print(sorted(result))                           # sort the list as we print it, otherwise it is in the order of list1
+print(sorted(result))                           # Sort the list as we print it, otherwise, it is in the order of list1
 
 # DICTIONARY COMPREHENSION
 # new_dict = {new_key: new_value for item in list}
 # new_dict = {new_key: new_value for item in list if test}
-# new_dict = {new_key: new_value for (key,value) in dict.items()}
-# new_dict = {new_key: new_value for (key,value) in dict.items() if test}
+# new_dict = {new_key: new_value for (key, value) in dict.items()}
+# new_dict = {new_key: new_value for (key, value) in dict.items() if test}
 import random
 names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
 student_scores = {name: random.randint(50, 100) for name in names}
 print(student_scores)     # {'Alex': 57, 'Beth': 58, 'Caroline': 63, 'Dave': 98, 'Eleanor': 60, 'Freddie': 65}
-passed_students = {student:score for (student,score) in student_scores.items() if score > 59}
+passed_students = {student: score for (student, score) in student_scores.items() if score > 59}
 print(passed_students)    # {'Caroline': 63, 'Dave': 98, 'Eleanor': 60, 'Freddie': 65}
 
 # CREATE A DICTIONARY THAT WILL PRINT EACH WORD OF A SENTENCE AND THE LENGTH OF THE WORD
@@ -67,7 +67,7 @@ result = {word:len(word) for word in sentence}
 print(result)
 # result = {'What': 4, 'is': 2, 'the': 3, 'Airspeed': 8, 'Velocity': 8, 'of': 2, 'an': 2, 'Unladen': 7, 'Swallow?': 8}
 
-# TAKE EACH temp in celsius AND CONVERT IT INTO temp in fahrenhiet
+# TAKE EACH temp in Celsius AND CONVERT IT INTO temp in Fahrenheit
 weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
 weather_f = {day:(temp_c * 9/5) + 32 for (day,temp_c) in weather_c.items()}
 print(weather_f)
